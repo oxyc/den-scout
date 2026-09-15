d1005690b5108436d1ee9747d020cc7771ad06f1
Oskar Schöldström <m@oxy.fi>
2026-09-15 11:18:43 -0300

    den-scout 0.23.0

--- internal/scout/manifest.go
+++ internal/scout/manifest.go
@@ -23,7 +23,7 @@ type manifestHints struct {
 	ConfigurationRequired bool `json:"configurationRequired"`
 }
 
-const manifestVersion = "0.22.0"
+const manifestVersion = "0.23.0"
 
 // buildManifest returns the manifest for a config (nil = unconfigured).
 func buildManifest(config *Config) manifestJSON {
